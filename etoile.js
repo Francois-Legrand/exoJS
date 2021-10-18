7dcafdfedad7655feae7bb1d0cde020763d224ce
Développeur web <51367556+Francois-Legrand@users.noreply.github.com>
2021-10-18 17:01:29 +0200

    update

--- etoile.js
+++ etoile.js
@@ -1,13 +1,23 @@
 let read = require('readline-sync');
 
+// let result = () => {
+//     let n = read.questionFloat("Donne moi un nombre ou un chiffe ");
+//     let filledArray = new Array(n).fill(' ');
+// while (filledArray.indexOf(' ') !== -1) {
+//         filledArray.shift();
+//         filledArray.push('#');
+//         console.log(filledArray.join(""));
+//         n--;
+        
+//     }
+
+
 let result = () => {
     let n = read.questionFloat("Donne moi un nombre ou un chiffe ");
-    let filledArray = new Array(n).fill(' ');
-while (filledArray.indexOf(' ') !== -1) {
+    let filledArray = new Array(n).fill('#');
+while (filledArray.indexOf('#') !== -1) {
         filledArray.shift();
-        filledArray.push('#');
         console.log(filledArray.join(""));
-        n--;
         
     }
     
