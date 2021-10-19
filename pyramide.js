e3bf4a732cca49748693ad25b41320e1133bf530
Développeur web <51367556+Francois-Legrand@users.noreply.github.com>
2021-10-19 08:48:37 +0200

    update

--- pyramide.js
+++ pyramide.js
@@ -1,17 +1,21 @@
 let read = require('readline-sync');
 
 let result = () => {
-    let n = read.questionFloat("Donne moi un nombre ou un chiffe ");
+    let n = read.questionInt("Donne moi un nombre ou un chiffe ");
     let filledArray = [];
     for(let i = 0; i < n; i++) {
         filledArray.splice(i,i,"#")
-
         console.log(filledArray.join(""));
-        if(filledArray.length == n){
-            while (filledArray.indexOf('#') !== -1) {
-                filledArray.shift();
-                console.log(filledArray.join(""));
         
+        if(filledArray.length == n){
+
+            // while (filledArray.indexOf('#') !== -1) {
+            //     filledArray.shift();
+            //     console.log(filledArray.join(""));
+            // }
+            for(let j = n; filledArray.length > 1; j--){
+                filledArray.pop();
+                console.log(filledArray.join(""));
             }
         }
     }
